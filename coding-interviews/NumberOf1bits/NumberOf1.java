7e7fe71670f6e451626a9b93accfe4261295f8ce
Pangm <swrok.x@gmail.com>
2016-04-03 10:58:56 +0800

    CI, Number of 1 bits.

---
+++ coding-interviews/NumberOf1bits/NumberOf1.java
@@ -0,0 +1,16 @@
+// #二进制中1的个数
+// 题目描述
+
+// 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+
+public class Solution {
+    public int NumberOf1(int n) {
+        int cnt = 0;
+        while (n != 0) {
+            n &= n - 1;
+            ++cnt;
+        }
+        
+        return cnt;
+    }
+}
